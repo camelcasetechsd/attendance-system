e284fd99c45322dad04104145e7f7aa875f1bee6
Adham Saad <adham.sa3d@gmail.com>
2015-08-12 15:03:04 +0200

    fix verify password function on php 5.3

--- library/Attendance/Entity/User.php
+++ library/Attendance/Entity/User.php
@@ -163,7 +163,7 @@ class User
         if (function_exists('password_verify')) {
             return password_verify($givenPassword, $savedPassword);
         } else {
-            return $this->hashPassword($givenPassword) == $savedPassword;
+            return self::hashPassword($givenPassword) == $savedPassword;
         }
     }
 
