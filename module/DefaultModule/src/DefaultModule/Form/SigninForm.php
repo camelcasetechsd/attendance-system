70199a2cd22876148b0353bfcb4c257fbdc81b90
Mohamed Labib <lebaz20@gmail.com>
2015-11-19 10:18:33 +0200

    ZF2 Migration - Add default module forms

---
+++ module/DefaultModule/src/DefaultModule/Form/SigninForm.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace DefaultModule\Form;
+
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilter;
+
+class SigninForm extends Form {
+
+    private $_inputFilter;
+
+    public function __construct($name = null, $options = null) {
+        parent::__construct($name, $options);
+
+        $this->setAttribute('class', 'form form-horizontal');
+
+        $this->add(array(
+            'name' => 'username',
+            'type' => 'Zend\Form\Element\Text',
+            'attributes' => array(
+                'required' => 'required',
+                'class' => 'form-control',
+            ),
+            'options' => array(
+                'label' => 'Username',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'password',
+            'type' => 'Zend\Form\Element\Password',
+            'attributes' => array(
+                'required' => 'required',
+                'class' => 'form-control',
+            ),
+            'options' => array(
+                'label' => 'Password',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'login',
+            'type' => 'Zend\Form\Element\Submit',
+            'attributes' => array(
+                'class' => 'btn btn-lg btn-success btn-block',
+            ),
+            'options' => array(
+                'label' => 'Log in',
+            ),
+        ));
+
+        $this->setInputFilter($this->getInputFilter());
+    }
+
+    private function getInputFilter() {
+        if (!$this->_inputFilter) {
+            $inputFilter = new InputFilter();
+
+            $inputFilter->add(array(
+                'name' => 'password',
+                'required' => true,
+                'filters' => array(
+                    array(
+                        'name' => 'StringTrim',
+                    )
+                )
+            ));
+
+            $inputFilter->add(array(
+                'name' => 'username',
+                'required' => true,
+                'filters' => array(
+                    array(
+                        'name' => 'StringTrim',
+                    )
+                ),
+                'validators' => array(
+                    array('name' => 'stringLength',
+                        'options' => array(
+                            'min' => 1,
+                            'max' => 25,
+                        )
+                    ),
+                )
+            ));
+
+            $this->_inputFilter = $inputFilter;
+        }
+
+        return $this->_inputFilter;
+    }
+
+}
