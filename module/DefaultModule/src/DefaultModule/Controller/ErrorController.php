49ea71c632dd113490a04725b0d43ce5eaf78e56
Mohamed Labib <lebaz20@gmail.com>
2015-11-25 16:16:21 +0200

    ZF2 Migration - Added old error handling, still not working yet

--- module/DefaultModule/src/DefaultModule/Controller/ErrorController.php
+++ module/DefaultModule/src/DefaultModule/Controller/ErrorController.php
@@ -2,10 +2,12 @@
 
 namespace DefaultModule\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Utilities\Controller\ActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Mvc\Application;
+use Zend\Log\Logger;
 
-class ErrorController extends AbstractActionController
+class ErrorController extends ActionController
 {
 
     public function indexAction()
@@ -20,9 +22,46 @@ class ErrorController extends AbstractActionController
 
     public function errorAction()
     {
-        return new ViewModel();
+        $errors = $this->_getParam('error_handler');
+        $variables = array();
+        if (!$errors || !$errors instanceof \ArrayObject) {
+            $variables['message'] = 'You have reached the error page';
+            return;
+        }
+        
+        switch ($errors->type) {
+            case Application::ERROR_CONTROLLER_NOT_FOUND:
+            case Application::ERROR_CONTROLLER_INVALID:
+            case Application::ERROR_ROUTER_NO_MATCH:
+                // 404 error -- controller or action not found
+                $this->getResponse()->setHttpResponseCode(404);
+                $priority = Logger::NOTICE;
+                $variables['message'] = 'Page not found';
+                break;
+            default:
+                // application error
+                $this->getResponse()->setHttpResponseCode(500);
+                $priority = Logger::CRIT;
+                $variables['message'] = 'Application error';
+                break;
+        }
+        
+        // Log exception
+        $log = new Logger();
+        $log->log($priority, $variables['message'], $errors->exception);
+        $log->log($priority, 'Request Parameters', $errors->request->getParams());
+        
+        // conditionally display exceptions
+        if ($this->getInvokeArg('displayExceptions') == true) {
+            $variables['exception'] = $errors->exception;
+            $variables['exceptionmessage'] = $variables['exception']->getMessage();
+            $variables['stacktrace'] = $variables['exception']->getTraceAsString();
+        }
+        
+        $variables['request']   = $errors->request;
+        $variables['requestparams'] = var_export($errors->request->getParams(), true);
+        
+        return new ViewModel($variables);
     }
-
-
 }
 
