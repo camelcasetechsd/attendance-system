b482aa6ffbb1ba2ba4789305d8843d49a453806e
Mohamed Labib <lebaz20@gmail.com>
2015-11-25 16:15:47 +0200

    ZF2 Migration - Added login and logout functionality

--- module/DefaultModule/src/DefaultModule/Controller/SignController.php
+++ module/DefaultModule/src/DefaultModule/Controller/SignController.php
@@ -2,10 +2,12 @@
 
 namespace DefaultModule\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Utilities\Controller\ActionController;
 use Zend\View\Model\ViewModel;
+use DefaultModule\Form\SigninForm;
+use Zend\Authentication\AuthenticationService;
 
-class SignController extends AbstractActionController
+class SignController extends ActionController
 {
 
     public function indexAction()
@@ -15,12 +17,38 @@ class SignController extends AbstractActionController
 
     public function inAction()
     {
-        return new ViewModel();
+        $variables = array();
+        $form = new SigninForm();
+        $request = $this->getRequest();
+        //checking if we got a new post request
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+            // checking if the form is valid
+            if ($form->isValid()) {
+                $auth = $this->getServiceLocator()->get('Users\Auth\Authentication')->setRequest($request);
+                $result = $auth->authenticateMe();
+                if ($result->isValid()) {
+                    $auth->newSession();
+                    $url = $this->getEvent()->getRouter()->assemble(array('action' => 'index'), array('name' => 'home'));
+                    $this->redirect()->toUrl($url);
+                } else {
+                    $errorMessages = array();
+                    $errorMessages[]['message'] = "Username and password are invalid !";
+                    $variables['message'] = $errorMessages;
+                }
+            }
+        }
+        $variables['form'] = $this->getFormView($form);
+        return new ViewModel($variables);
     }
 
     public function outAction()
     {
-        return new ViewModel();
+        $auth = new AuthenticationService();
+        $auth->clearIdentity();
+        //Redirect to login page again 
+        $url = $this->getEvent()->getRouter()->assemble(array('action' => 'in'), array('name' => 'defaultSign'));
+        $this->redirect()->toUrl($url);
     }
 
 
