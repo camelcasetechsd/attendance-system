bb5178b0b39d415bebdb7d0b38ed14a600d7090c
Mohamed Labib <lebaz20@gmail.com>
2015-11-30 10:17:54 +0200

    ZF2 Migration - removed commented code not needed anymore

--- module/DefaultModule/src/DefaultModule/Controller/IndexController.php
+++ module/DefaultModule/src/DefaultModule/Controller/IndexController.php
@@ -4,21 +4,12 @@ namespace DefaultModule\Controller;
 
 use Utilities\Controller\ActionController;
 use Zend\View\Model\ViewModel;
-use Mustache_Engine;
 
 class IndexController extends ActionController
 {
 
     public function indexAction()
     {
-//        $m = new Mustache_Engine([
-//  'pragmas' => [Mustache_Engine::PRAGMA_BLOCKS],
-//  'partials' => [
-//    'parent' => 'Hello {{$ content }}planet{{/ content }}'
-//  ],
-//]);
-//
-//echo $m->render('{{< parent }}{{$ content }}World!{{/ content }}{{/ parent }}', []);die;
         return new ViewModel();
     }
 
