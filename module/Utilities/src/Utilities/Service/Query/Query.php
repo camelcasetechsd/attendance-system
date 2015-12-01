16fd2a8560de0b16f5cacf5fce013658db9c2ef3
Mohamed Labib <lebaz20@gmail.com>
2015-12-01 19:01:58 +0200

    ZF2 Migration - Added support for saving entities with associations set with only id

--- module/Utilities/src/Utilities/Service/Query/Query.php
+++ module/Utilities/src/Utilities/Service/Query/Query.php
@@ -1,27 +1,30 @@
 <?php
+
 namespace Utilities\Service\Query;
 
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\Collections\Criteria;
 
-class Query
-{
+class Query {
+
     /**
      *
      * @var ObjectManager 
      */
     public $entityManager;
+
     /**
      *
      * @var Doctrine\Common\Persistence\ObjectRepository 
      */
     public $entityRepository;
+
     /**
      *
      * @var string 
      */
     public $entityName;
-    
+
     /**
      * 
      * @param ObjectManager $entityManager
@@ -35,13 +38,13 @@ class Query
      * @param string $entityName
      */
     public function setEntity($entityName) {
-        if(! empty($entityName)){
+        if (!empty($entityName)) {
             $this->entityName = $entityName;
             $this->entityRepository = $this->entityManager->getRepository($entityName);
         }
         return $this;
     }
-    
+
     /**
      * Finds an entity by its primary key / identifier.
      *
@@ -49,8 +52,7 @@ class Query
      *
      * @return object|null The entity instance or NULL if the entity can not be found.
      */
-    public function find($entityName, $id)
-    {
+    public function find($entityName, $id) {
         return $this->setEntity($entityName)->entityRepository->find($id);
     }
 
@@ -59,8 +61,7 @@ class Query
      *
      * @return array The entities.
      */
-    public function findAll($entityName)
-    {
+    public function findAll($entityName) {
         return $this->setEntity($entityName)->entityRepository->findAll();
     }
 
@@ -74,8 +75,7 @@ class Query
      *
      * @return array The objects.
      */
-    public function findBy($entityName, array $criteria, array $orderBy = null, $limit = null, $offset = null)
-    {
+    public function findBy($entityName, array $criteria, array $orderBy = null, $limit = null, $offset = null) {
         return $this->setEntity($entityName)->entityRepository->findBy($criteria, $orderBy, $limit, $offset);
     }
 
@@ -87,29 +87,48 @@ class Query
      *
      * @return object|null The entity instance or NULL if the entity can not be found.
      */
-    public function findOneBy($entityName, array $criteria, array $orderBy = null)
-    {
+    public function findOneBy($entityName, array $criteria, array $orderBy = null) {
         return $this->setEntity($entityName)->entityRepository->findOneBy($criteria, $orderBy);
     }
-    
-    public function filter($entityName, $criteria = false, $countFlag = false)
-    {
-        if(! $criteria instanceof Criteria){
+
+    public function filter($entityName, $criteria = false, $countFlag = false) {
+        if (!$criteria instanceof Criteria) {
             $criteria = new Criteria();
         }
         $return = $this->setEntity($entityName)->entityRepository->matching($criteria);
-        if($countFlag === true){
-           $return = (int) $return->count();
+        if ($countFlag === true) {
+            $return = (int) $return->count();
         }
         return $return;
     }
-    
-    public function save($entity, $data = array())
-    {
-        if(! empty($data) && method_exists($entity, 'exchangeArray')){
+
+    public function save($entity, $data = array()) {
+        // if association hold id not actual object, 
+        // then find that object to set the corresponding property with it
+        $classMetadata = $this->entityManager->getClassMetadata($this->entityName);
+        $associationNames = $classMetadata->getAssociationNames();
+        foreach ($associationNames as $associationName) {
+            if (isset($data[$associationName])) {
+                $currentValue = $data[$associationName];
+            } else {
+                $currentValue = $entity->$associationName;
+            }
+            if (!is_object($currentValue) && is_numeric($currentValue)) {
+                $targetClass = $classMetadata->getAssociationTargetClass($associationName);
+                $currentValue = $this->find($targetClass, $currentValue);
+                if (isset($data[$associationName])) {
+                    $data[$associationName] = $currentValue;
+                } else {
+                    $entity->$associationName = $currentValue;
+                }
+            }
+        }
+        
+        if (!empty($data) && method_exists($entity, 'exchangeArray')) {
             $entity->exchangeArray($data);
         }
         $this->entityManager->persist($entity);
         $this->entityManager->flush($entity);
     }
+
 }
