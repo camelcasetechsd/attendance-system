654dd5e3ae9c6dc20ad1a299b41e7901475b5d08
Mohamed Labib <lebaz20@gmail.com>
2015-12-06 13:25:54 +0200

    ZF2 Migration - Add calender module documentation

--- module/Calendar/src/Calendar/Controller/IndexController.php
+++ module/Calendar/src/Calendar/Controller/IndexController.php
@@ -5,9 +5,23 @@ namespace Calendar\Controller;
 use Utilities\Controller\ActionController;
 use Zend\View\Model\ViewModel;
 
+/**
+ * Calender Index controller
+ * 
+ * controller responsible for displaying holiday calender
+ * 
+ * @author Mohamed Labib <mohamed.labib@camelcasetech.com>
+ */
 class IndexController extends ActionController
 {
 
+    /**
+     * Display holiday calender 
+     * @author Mohamed Labib <mohamed.labib@camelcasetech.com>
+     * 
+     * @access public
+     * @return ViewModel
+     */
     public function indexAction()
     {
         return new ViewModel();
